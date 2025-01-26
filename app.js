497fc257b122ea6dc84060f4216900c61248f74b
Ahsan Azam <aazam@teresol.com>
2025-01-26 19:30:55 +0500

    work onusers events and booking api

--- app.js
+++ app.js
@@ -1,64 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql')
+const mongoose = require('mongoose');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index')
+
 
 const App = express();
-
-const events = [];
 
 App.use(bodyParser.json());
 
 App.use('/graphqlApi', graphqlHTTP({
-    schema: buildSchema(`
-
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-        
-         input EventInput { 
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        
-        type rootQuery {
-            events: [Event!]!
-        
-        }
-
-        type rootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-
-        schema {
-            query: rootQuery
-            mutation: rootMutation
-        }
-        `),
-    rootValue: {
-        events: () => {
-            return events
-        },
-        createEvent: (args) => {
-
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event);
-            return event;
-        }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
 }));
-App.listen(3000)
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
+    }@jobs-app-db.gvv6l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=jobs-app-db`)
+    .then(() => {
+        App.listen(3000);
+    }).catch(err => {
+        console.log(err);
+
+    });
